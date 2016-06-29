ef7bc876dc662a4f00fdca8ac4dce6b514e0d7d1
yuntao.qyt <yuntao.qyt@alibaba-inc.com>
2016-06-29 23:20:22 +0800

    328.js

---
+++ tree/230.js
@@ -0,0 +1,37 @@
+/**
+ * link: https://leetcode.com/problems/kth-smallest-element-in-a-bst/
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} k
+ * @return {number}
+ */
+var kthSmallest = function(root, k) {
+    if (!root) return root;
+    var array = [];
+    array = getSortArray(root, array);
+
+    return array[k];
+};
+
+function getSortArray(root, array) {
+    if (!root) return array;
+    if (!root.left) {
+        array.push(root.val);
+    } else {
+        array = getSortArray(root.left, array);
+        array.push(root.val);
+    }
+
+    if (!root.right) {
+        return array;
+    } else {
+        array = getSortArray(root.right, array);
+    }
+    return array;
+}
