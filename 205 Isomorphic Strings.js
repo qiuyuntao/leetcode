83dcbbda63cbb446a85e77490b066966dd903d24
yuntao.qyt <yuntao.qyt@alibaba-inc.com>
2016-07-12 22:14:17 +0800

    205

--- 205 Isomorphic Strings.js
+++ 205 Isomorphic Strings.js
@@ -1,34 +1,29 @@
 /**
+ * link: https://leetcode.com/problems/isomorphic-strings/
  * @param {string} s
  * @param {string} t
  * @return {boolean}
  */
 var isIsomorphic = function(s, t) {
-  var temp_s = '';
-  var temp_t = '';
   var map_s = {};
-  var index_s = 1;
   var map_t = {};
-  var index_t = 1;
+
   for (var i = 0; i < s.length; i++) {
     var a = s[i];
-    var b = t[i]
-    if (!map_s[a]) map_s[a] = index_s++;
-    temp_s += map_s[a];
+    var b = t[i];
 
-    if (!map_t[b]) map_t[b] = index_t++;
-    temp_t += map_t[b];
+    if (!map_s[a]) {
+      map_s[a] = b;
+    } else if (map_s[a] !== b) {
+      return false;
+    }
 
-    if (temp_t !== temp_s) return false;
+    if (!map_t[b]) {
+      map_t[b] = a;
+    } else if (map_t[b] !== a) {
+      return false;
+    }
   }
 
   return true;
 };
-
-var s = 'aa';
-var t = 'ab';
-
-
-
-
-console.log(isIsomorphic(s, t))
