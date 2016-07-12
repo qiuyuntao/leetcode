51e3ac090f73b4c54056a2c2c0cd00420777ddce
qiuyuntao <yuntao.qyt@alibaba-inc.com>
2016-07-13 00:45:18 +0800

    34

--- util.js
+++ util.js
@@ -24,5 +24,20 @@ const U = {
       head = head.next;
     }
     return array;
+  },
+  binarySearch(arr, val) {
+    var high = arr.length - 1;
+    var mid = 0;
+    while (low <= high) {
+      mid = Math.floor((low + high) / 2);
+      if (arr[mid] === val) {
+        return mid;
+      } else if (arr[mid] > val) {
+        high = mid - 1;
+      } else {
+        low = mid + 1;
+      }
+    }
+    return -1;
   }
 }
